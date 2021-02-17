e39ecca660076c6962fadc24d39565eeb92f5307
Eric <EricJB77@gmail.com>
2021-02-17 02:54:26 -0500

    Added more quotes and added feature to add your own quotes through an added menu system

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,29 @@
 import random
 
-def primary():
-  #print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
+def randomquote(quotes):
   last = len(quotes) -1
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  print("Random Quote: ",quotes[rnd])
 
-  
 
-if __name__== "__main__":
-  primary()
+f = open("quotes.txt")
+quotes = f.readlines()
+f.close()
+
+selection = "A"
+
+while True:
+  selection = input("(D)isplay a quote\n(A)dd a quote\nChoose your selection by typing in the letter: ")
+  if selection.upper() == "D":
+    randomquote(quotes)
+  elif selection.upper() == "A":
+    quotetoadd = input("What saying would you like to add to the file?: ")
+    filewritingto = open("quotes.txt",'a')
+    filewritingto.write(quotetoadd + "\n")
+    filewritingto.close()
+    print("'",quotetoadd,"' has been written to the file!")
+  else:
+    print("Exiting...")
+    break
+
+
